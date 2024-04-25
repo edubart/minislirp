ef3ee7396c0d9c96f2100ec6e1c83a6921f6b5be
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-04-25 20:10:57 +0200

    oss-fuzz: Fix udp-h and tcp-h corpuses

--- fuzzing/oss-fuzz.sh
+++ fuzzing/oss-fuzz.sh
@@ -28,11 +28,12 @@ ninja -C $build
 
 zip -jqr $OUT/fuzz-ip-header_seed_corpus.zip "$(dirname "$0")/IN_ip-header"
 zip -jqr $OUT/fuzz-udp_seed_corpus.zip "$(dirname "$0")/IN_udp"
-zip -jqr $OUT/fuzz-udp_h_seed_corpus.zip "$(dirname "$0")/IN_udp-h"
+zip -jqr $OUT/fuzz-udp-h_seed_corpus.zip "$(dirname "$0")/IN_udp-h"
 zip -jqr $OUT/fuzz-tftp_seed_corpus.zip "$(dirname "$0")/IN_tftp"
 zip -jqr $OUT/fuzz-dhcp_seed_corpus.zip "$(dirname "$0")/IN_dhcp"
 zip -jqr $OUT/fuzz-icmp_seed_corpus.zip "$(dirname "$0")/IN_icmp"
 zip -jqr $OUT/fuzz-tcp_seed_corpus.zip "$(dirname "$0")/IN_tcp"
+zip -jqr $OUT/fuzz-tcp-h_seed_corpus.zip "$(dirname "$0")/IN_tcp-h"
 
 find $build -type f -executable -name "fuzz-*" -exec mv {} $OUT \;
 find $build -type f -name "*.options" -exec mv {} $OUT \;
