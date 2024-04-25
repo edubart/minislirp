c76d880a2cb5460132dab20eaac6867f9196df98
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-04-25 16:25:59 +0200

    udp: Make cleanup loop clearer
    382bff02f21b ("Fix possible infinite loops and use-after-free") rewrote
    the loop to make it cleaner that we go through items, but was always
    detaching the first elements, which happens to be right, but less
    clear than detaching the element being processed.
    
    Fixes #32
    
    Reported-by: Peter Maydell

--- src/udp.c
+++ src/udp.c
@@ -56,7 +56,7 @@ void udp_cleanup(Slirp *slirp)
 
     for (so = slirp->udb.so_next; so != &slirp->udb; so = so_next) {
         so_next = so->so_next;
-        udp_detach(slirp->udb.so_next);
+        udp_detach(so);
     }
 }
 
